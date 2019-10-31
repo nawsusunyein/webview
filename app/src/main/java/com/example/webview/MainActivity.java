b4de666d99ff7bb94c48c15bfc9761116850c664
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-10-31 16:29:11 +0630

    inserting webview

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.journaldev.com");
+    }
+}
